2b824a381e0939c281909867608a9354dbf8c763
Eric Armbruster <eric.armbruster@tum.de>
2023-06-22 11:23:12 +0200

    Use multiprocessing to speed up image generation

--- extraction/annotated_images.py
+++ extraction/annotated_images.py
@@ -1,6 +1,8 @@
+from itertools import repeat
 import os
+import logging
+import multiprocessing
 
-from tqdm import tqdm
 from typing import List
 from extraction.helpers import get_class_list
 from vod.common.file_handling import get_frame_list_from_folder
@@ -8,26 +10,32 @@ from vod.configuration.file_locations import KittiLocations
 from vod.frame.data_loader import FrameDataLoader
 from vod.visualization.vis_2d import Visualization2D
 
+
+def gen_annotation(frame_number: str, kitti_locations: KittiLocations, modalities: List[bool], outdir):
+    if os.path.exists(f'{dir}/{frame_number}.png'):
+        return
+        
+    lidar, radar = modalities
+    loader = FrameDataLoader(
+        kitti_locations=kitti_locations, frame_number=frame_number)
+
+    vis2d = Visualization2D(frame_data_loader=loader, classes_visualized=get_class_list())
+    vis2d.draw_plot(plot_figure=False, save_figure=True, show_gt=True,
+                    show_lidar=lidar, show_radar=radar, outdir=outdir)
+
 def generate_annotated_images(kitti_locations: KittiLocations, 
                               outdir: str, 
                               lidar: bool = True, 
-                              radar: bool = True, 
-                              classes_visualized: List[str]=get_class_list()):
+                              radar: bool = True):
     
     dir = f'{kitti_locations.output_dir}/{outdir}'
     os.makedirs(dir, exist_ok=True)
 
     frames = get_frame_list_from_folder(kitti_locations.label_dir, '.txt')
-    for frame_number in tqdm(frames, desc="Generating annotated images"):
-        if os.path.exists(f'{dir}/{frame_number}.png'):
-            continue
-        
-        loader = FrameDataLoader(
-            kitti_locations=kitti_locations, frame_number=frame_number)
-
-        vis2d = Visualization2D(frame_data_loader=loader, classes_visualized=classes_visualized)
-        vis2d.draw_plot(plot_figure=False, save_figure=True, show_gt=True,
-                        show_lidar=lidar, show_radar=radar, outdir=outdir)
+    iter = zip(frames, repeat(kitti_locations), repeat([lidar, radar]), repeat(outdir))
+    pool = multiprocessing.Pool(processes=12)
+    
+    r = list(pool.starmap(gen_annotation, iter), desc="Generating annotated images")
         
         
 def generate_all_annotated_images(kitti_locations: KittiLocations):
